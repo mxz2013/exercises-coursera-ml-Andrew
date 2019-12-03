436f7f12290af0397747153977195d7c79544a69
mxz2013 <jqzhou.polytechnique@gmail.com>
2019-12-03 15:47:34 +0100

    finish ex5

--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -38,9 +38,16 @@ error_val = zeros(length(lambda_vec), 1);
 %       end
 %
 %
+m = size(X, 1);
+n = size(Xval, 1);
+X = [ones(m, 1) X] ;
+Xval = [ones(n, 1) Xval] ;
 
-
-
+for i = 1:length(lambda_vec)
+	lambda = lambda_vec(i);
+	theta = trainLinearReg(X, y , lambda) ; 
+        error_train(i) = linearRegCostFunction(X, y, theta, 0)  ;
+	error_val(i)  = linearRegCostFunction(Xval, yval, theta, 0) ;
 
 
 
